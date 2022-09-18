11df67881d4414e5abe7d97a04fb576528a5a95c
Roman-Bogatikov <fox1323@mail.ru>
2022-09-18 19:22:53 +0300

    l2ex7 script fixed

--- L2ex7_requests_and_methods.py
+++ L2ex7_requests_and_methods.py
@@ -3,11 +3,11 @@ import requests
 link = "https://playground.learnqa.ru/ajax/api/compare_query_type"
 get_params = {"method": "GET"}
 response_one = requests.get(link)
-print(f"Ответ при отсутствующем парметре method: {response_one.text}")
+print(f"Ответ при отсутствующем параметре method: {response_one.text}")
 wrong_method_answer = response_one.text
 
 response_two = requests.head(link)
-print(f"Ответ при изпользовании некорректного типа запроса статус код: {response_two.status_code}")
+print(f"Ответ при использовании некорректного типа запроса, статус код: {response_two.status_code}")
 
 response_three = requests.get(link, params=get_params)
 print(f"Ответ при использовании корректного параметра method: {response_three.text}")
@@ -20,7 +20,7 @@ for method_type in dict_of_method:
         formatting_param = {"{}".format('params' if method_type == 'GET' else 'data'): {"method": param}}
         request = requests.request(method_type, link, **formatting_param)
         message = f"Некорректный ответ! Метод запроса: {method_type}, " \
-                  f"параметр: {formatting_param}, ответ: {request.text}, {request.status_code}"
+                  f"параметр: {formatting_param}, ответ: {request.text}"
         if method_type == param and request.text != success_answer:
             print(message)
         elif method_type != param and request.text == success_answer:
