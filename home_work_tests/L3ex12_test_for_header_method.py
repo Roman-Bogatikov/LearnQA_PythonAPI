8bf42b968ac626bc9551f9bb3173395cc109528f
Roman-Bogatikov <fox1323@mail.ru>
2022-09-27 08:52:39 +0300

    l3ex12 script added

---
+++ home_work_tests/L3ex12_test_for_header_method.py
@@ -0,0 +1,12 @@
+import requests
+from lib.base_case import BaseCase
+
+class TestForHeaderMethod (BaseCase):
+    def test_for_header_method(self):
+        link = "https://playground.learnqa.ru/api/homework_header"
+        response = requests.get(link)
+        print(response.headers)
+        header = "x-secret-homework-header"
+        expected_header_value = "Some secret value"
+        response_header_value = self.get_header(response, header)
+        assert response_header_value == expected_header_value, f"Response header {header} isn't correct"
